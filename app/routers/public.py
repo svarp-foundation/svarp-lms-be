bc68e93243b1f5a0d27252dc8e2beeefe5837d4f
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-03-07 22:56:30 +0530

    hot fix

--- app/routers/public.py
+++ app/routers/public.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from .. import models, schemas, database
 
 router = APIRouter(
