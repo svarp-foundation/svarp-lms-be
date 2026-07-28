51da3e2bc0737845b6d08f2e70a50ba742d5e48b
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-07-28 07:26:57 +0530

    feat: import Query and Optional to support optional parameters in admin routes

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File, BackgroundTasks
+from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File, BackgroundTasks, Query
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 import shutil
 import os
 import uuid
